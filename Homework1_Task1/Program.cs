abb169fe3bf24d4d048fff7dbfa181390706a8cf
Tigran <armenhayrapetyan78@gmail.com>
2024-09-23 23:22:03 +0400

    Homework1_Task1 done successfully

--- Homework1_Task1/Program.cs
+++ Homework1_Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа для сравнения: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Числа равны!");
+}
+else 
+{
+    Console.WriteLine(num2);
+}
