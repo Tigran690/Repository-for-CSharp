0804ce2ee24a93fe48599c7054fe0e0d3cf8c216
Tigran <armenhayrapetyan78@gmail.com>
2024-09-29 11:38:40 +0400

    Task2 done successfully

--- Homework1_Task2/Program.cs
+++ Homework1_Task2/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Formats.Asn1;
+
+Console.WriteLine("Введите 3 числа для сравнения. При завершении будет выведена самая большая цифра:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    Console.WriteLine(a);
+    else if (c > a)
+    Console.WriteLine(c);
+}
+else if (a < b)
+{
+    if (b > c)
+    Console.WriteLine(b);
+    else if (b < c)
+    Console.WriteLine (c);
+}
+// if (a > c)
+// {
+//     Console.WriteLine(a);
+// }
+// else if (b > c)
+// {
+//     Console.WriteLine(b);
+// }
+// else
+// {
+//     Console.WriteLine(c);
+// }
