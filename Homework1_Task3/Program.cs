249f1482e332454f812648d836bd7e6883b0c151
Tigran <armenhayrapetyan78@gmail.com>
2024-09-29 12:30:08 +0400

    Task3 done successfully

--- Homework1_Task3/Program.cs
+++ Homework1_Task3/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число, которое будет проверено: чётное или нет?");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 2;
+
+int d = a%b;
+
+if (d == 1)
+{
+    Console.WriteLine("Число нечётное");
+}
+else
+{
+    Console.WriteLine("Число чётное");
+}
