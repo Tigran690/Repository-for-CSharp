1a84aa5ba8db6e64c18445d6f1c9f664c59bd691
Tigran <armenhayrapetyan78@gmail.com>
2024-09-29 12:53:23 +0400

    Task4 done successfully

--- Homework1_Task4/Program.cs
+++ Homework1_Task4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести все чётные числа, расположенные от 0 до цифры N
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
