2b6772dbb63933fbe279506b02d8a508cd0b06ec
Tigran <armenhayrapetyan78@gmail.com>
2024-10-07 22:51:20 +0400

    Homework2_Task2 done successfully

--- Homework2_Task2/Program.cs
+++ Homework2_Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки(X и Y), причём
+// X не 0, Y не 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+// 2, 3 => 1
+// -5, 3 => 2
+// -3, -2 => 3
+// 4, -2 => 4
+Console.WriteLine("Введите координаты точки(X, Y), чтобы узнать в какой координатной четверти плоскости расположено оно:");
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+Console.WriteLine("4");
+}
+else
+{
+Console.WriteLine("Некорректные координаты!");
+}
