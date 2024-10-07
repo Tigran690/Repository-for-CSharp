c9111f46c687aa6c21d5fb9c2ebdc8f7a8a4eafe
Tigran <armenhayrapetyan78@gmail.com>
2024-10-07 22:16:49 +0400

    Homework2_Task1 done successfully

--- Homework2_Task1/Program.cs
+++ Homework2_Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// a = 50 => нет
+// a = 7 => нет
+// a = 322 => да
+Console.WriteLine("Введите число для проверки(кратно ли оно одновременно 7 и 23?):");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0)
+{
+    if (number % 23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
