2bcdbda16a9fdc5ca2b5af9459afed8f2b7bfafc
Tigran <armenhayrapetyan78@gmail.com>
2024-10-09 12:20:10 +0400

    Homework3_Task3 done successfully

--- Homework3_Task3/Program.cs
+++ Homework3_Task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// массив [2.2 0.4 9.11 7.2 78.98] => 78.58
+// массив [1.22 4.5 3.33] => 3.28
+
+double[] array = new double[] {1.22, 4.5, 3.33};
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
