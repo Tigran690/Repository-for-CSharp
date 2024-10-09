d2e9eb8d05c8b59bc076ba4f54d9f3f837e78656
Tigran <armenhayrapetyan78@gmail.com>
2024-10-09 11:33:47 +0400

    Homework3_Task1

--- Homework3_Task1/Program.cs
+++ Homework3_Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите кол-во элементов массива, значения которых лежат в отрезке [10, 90]
+// массив [10, 21, 14, 93, 23] => 4
+
+int[] numbers = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+int count = 0;
+foreach (int number in numbers)
+{
+    if (number >= 10 && number <= 90)
+    {
+        count++;
+    }
+}         
+Console.WriteLine (count);
