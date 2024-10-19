15ce78fe604cb8ca53576cb72e111df0fdb79d97
Tigran <armenhayrapetyan78@gmail.com>
2024-10-19 20:28:10 +0400

    Homework4_Task2 done successfully

--- Homework4_Task2/Program.cs
+++ Homework4_Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными
+// трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в
+// массиве.
+// [344 452 341 125] => 2
+
+int[] numbers = new int[10];
+Random random = new Random();
+int evenCount = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(100, 1000);
+    Console.Write(numbers[i] + " ");
+    if (numbers[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+Console.WriteLine($"\n Кол-во четных чисел в массиве: {evenCount}");
