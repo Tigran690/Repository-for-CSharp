523a7bb914abaac2e3972dd76629f0c6555de431
Tigran <armenhayrapetyan78@gmail.com>
2024-10-19 18:54:13 +0400

    Homework4_Task1 done successfully

--- Homework4_Task1/Program.cs
+++ Homework4_Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая бесконечно
+// запрашивает целые числа с консоли. Программа
+// завершается при вводе символа ‘q’ или при вводе
+// числа, сумма цифр которого четная.
+// 5 12 16 q [STOP]
+// 3 45 342 15 [STOP] 
+
+while (true)
+{
+    Console.Write("Введите число или 'q' для выхода: ");
+    string input = Console.ReadLine();
+    if (input == "q")
+    {
+        Console.WriteLine("STOP. Программа завершена");
+        break;
+    }
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("STOP");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Введите целое число или 'q' для выхода.");
+    }
+}
+
