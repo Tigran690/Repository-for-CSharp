ac9ef799bc98cf40d5309bc922357c747cdfec43
Tigran <armenhayrapetyan78@gmail.com>
2024-10-20 16:10:17 +0400

    Homework5_Task1 done successfully

--- Homework5_Task1/Program.cs
+++ Homework5_Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Пример
+// 4 3 1 (1,2) => 9
+// 2 6 9 
+
+static void FindMaxInRows(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        int max = array[i, 0];
+        for (int j = 1; j < cols; j++)
+        {
+            if (array[i, j] > max)
+            {
+                max = array[i, j];
+            }
+        }
+        Console.WriteLine($"Строка № {i}");
+        Console.WriteLine($"Максимальный элемент строки № {i} :{max}");
+    }
+}
+int[,] array =
+{
+    {3, 2, 8, 5},
+    {4, 7, 6, 1},
+};
+FindMaxInRows(array);
