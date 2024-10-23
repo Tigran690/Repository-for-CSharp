5f5b98b7996d4e9932243275d6d35506402270ed
Tigran <armenhayrapetyan78@gmail.com>
2024-10-23 20:05:31 +0400

    Homework6_Task3 done successfully

--- Homework6_Task3/Program.cs
+++ Homework6_Task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+// Пример
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+class Task3
+{
+    static void Main(string[] args)
+    {
+        string input = "aBcD1ef!-";
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine(isPalindrome ? "Да" : "Нет");
+    }
+    static bool IsPalindrome(string str)
+    {
+        string normalized = new
+        string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+        return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
