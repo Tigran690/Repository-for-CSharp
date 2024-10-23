9a8bc8aafb0e78f56d9f5ea77b33f46b1a2e67f7
Tigran <armenhayrapetyan78@gmail.com>
2024-10-23 19:12:34 +0400

    Homework6_Task2 done successfully

--- Homework6_Task2/Program.cs
+++ Homework6_Task2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные.
+// Пример
+// “aBcD1ef!-” => “abcd1ef!-”
+
+string input = "aBcD1ef!-";
+string result = input.ToLower();
+Console.WriteLine(result);
