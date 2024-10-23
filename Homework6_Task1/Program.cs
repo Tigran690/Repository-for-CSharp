96618650f32974d2e06e147fb9d12ed152ace90b
Tigran <armenhayrapetyan78@gmail.com>
2024-10-23 13:00:36 +0400

    Homework6_Task1 done successfully

--- Homework6_Task1/Program.cs
+++ Homework6_Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив символов (тип char[,]).
+// Создать строку из символов этого массива. 
+// Пример
+// a b c => “abcdef”
+// d e f 
+
+string CharArrayToString(char[] arr)
+{
+    string res = string.Empty;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+char[] array = {'a', 'b', 'c', 'd', 'e', 'f'};
+string result = CharArrayToString(array);
+Console.WriteLine(result);
