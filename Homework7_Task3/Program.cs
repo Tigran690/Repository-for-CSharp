e463efdee583665c1543c219e54abbfd3713640c
Tigran <armenhayrapetyan78@gmail.com>
2024-10-26 21:04:57 +0400

    Homework7_Task3 done successfully

--- Homework7_Task3/Program.cs
+++ Homework7_Task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// Пример
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+using System;
+
+class Task3
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+        
+        Console.WriteLine("Переворачиваем:");
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void PrintArrayReverse(int[] array, int i)
+    {
+        if (i < 0) return;
+        Console.Write(array[i] + " ");
+        PrintArrayReverse(array, i - 1);
+    }
+}
